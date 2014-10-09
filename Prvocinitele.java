99ea06b0a622c1fda374f8c88ff63f921a16729b
Jaroslav Nejedly <jardanejedly@hotmail.com>
2014-10-09 16:08:38 +0200

    Prvocinitele, i zaporna cisla (fixes #1)

--- Prvocinitele.java
+++ Prvocinitele.java
@@ -6,46 +6,55 @@ public class Prvocinitele {
 		while (scanner.hasNextInt()) {
 			int cislo = scanner.nextInt();
 			
-			if (cislo < 1) {
-				System.out.printf("%d neresim.\n", cislo);
+			if (cislo == 0) {
+				System.out.printf("Koncim.");
+				break;
+			}
+      if (cislo == 1) {
+				System.out.printf("%d = 1\n", cislo);
 				continue;
 			}
-
-			System.out.printf("%d =", cislo);
+			if (cislo < 0) {
+				cislo *= -1;
+        System.out.printf("-%d = -1", cislo);
+			}
+			else {
+				System.out.printf("%d =", cislo);
+			}
 			Prvocinitel(cislo);
 		}
 	}
-  
-  public static void Prvocinitel(int n)
-  {
-      int delitel = 2;
+
+	public static void Prvocinitel(int n)
+	{
+			int delitel = 2;
 			while (delitel <= n) {
 				if ((n % delitel) == 0) {
-          if(IsPrime(delitel))
-          {
-            System.out.printf(" %d", delitel);
-          }
-          else
-          {
-            Prvocinitel(delitel);            
-          }
+					if(IsPrime(delitel))
+					{
+						System.out.printf(" %d", delitel);
+					}
+					else
+					{
+						Prvocinitel(delitel);
+					}
 					n /= delitel;
-          continue;
+					continue;
 				}
 				delitel++;
 			}
 			System.out.println();
-  }
-  
-  public static Boolean IsPrime(int n)
-  {
-    for(int i = 2; i < n / 2; i++)
-    {
-      if(n % i == 0)
-      {
-        return false;
-      }
-    }
-    return true;
-  }
+	}
+
+	public static Boolean IsPrime(int n)
+	{
+		for(int i = 2; i < n / 2; i++)
+		{
+			if(n % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
