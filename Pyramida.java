123e21a0fa0af20ca66cc1417f9c79507fd76ca4
Jaroslav Nejedly <jardanejedly@hotmail.com>
2014-10-09 15:06:43 +0200

    Pyramida

--- Pyramida.java
+++ Pyramida.java
@@ -1,11 +1,15 @@
-/* Nakresli (krasnou) pyramidu. */
+/* Nakresli pyramidu. */
 public class Pyramida {
 	public static void main(String[] args) {
 		int vyska = Integer.parseInt(args[0]);
 		
 		for (int y = 0; y < vyska; y++) {
+      for (int x = vyska - y; x > 0; x-- )
+      {
+        System.out.print(" ");
+      }
 			for (int x = 0; x < y * 2 + 1; x++) {
-				System.out.printf("-");
+				System.out.printf("X");
 			}
 			System.out.println();
 		}
