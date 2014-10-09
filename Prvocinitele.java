17c0ed9664a7512ea1e121f8b0c1da048d7f5e79
Jaroslav Nejedly <jardanejedly@hotmail.com>
2014-10-09 15:23:23 +0200

    Prvocinitele

--- Prvocinitele.java
+++ Prvocinitele.java
@@ -10,17 +10,42 @@ public class Prvocinitele {
 				System.out.printf("%d neresim.\n", cislo);
 				continue;
 			}
-			
+
 			System.out.printf("%d =", cislo);
-			int delitel = 2;
-			while (delitel <= cislo) {
-				if ((cislo % delitel) == 0) {
-					System.out.printf(" %d", delitel);
-					cislo /= delitel;
+			Prvocinitel(cislo);
+		}
+	}
+  
+  public static void Prvocinitel(int n)
+  {
+      int delitel = 2;
+			while (delitel <= n) {
+				if ((n % delitel) == 0) {
+          if(IsPrime(delitel))
+          {
+            System.out.printf(" %d", delitel);
+          }
+          else
+          {
+            Prvocinitel(delitel);            
+          }
+					n /= delitel;
+          continue;
 				}
 				delitel++;
 			}
 			System.out.println();
-		}
-	}
+  }
+  
+  public static Boolean IsPrime(int n)
+  {
+    for(int i = 2; i < n / 2; i++)
+    {
+      if(n % i == 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
 }
